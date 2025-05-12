619d8278aa6c278a5c1235219804f42df72331f7
Fernan Buan <fernan.buan@hoolisoftware.com>
2025-05-12 20:06:33 +0800

    refactor: [alert] add implicit assertive on examples

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export const Alert = () => {
     const [isAlertVisible, setIsAlertVisible] = useState(false);
     const [isAriaLivePoliteVisible, setIsAriaLivePoliteVisible] = useState(false);
+    const [isAriaLiveAssertiveVisible, setIsAriaLiveAssertiveVisible] = useState(false);
 
     return (
         <>
@@ -43,7 +44,7 @@ export const Alert = () => {
                     className="p-3 border-2 rounded-sm"
                     onClick={() => setIsAriaLivePoliteVisible(true)}
                 >
-                    Trigger Alert by ARIA-Live
+                    Trigger Alert by ARIA-Live (polite)
                 </button>
                 <div
                     aria-live="polite"
@@ -61,6 +62,37 @@ export const Alert = () => {
                                 className="p-3 border-2 rounded-sm"
                                 aria-label="Close"
                                 onClick={() => setIsAriaLivePoliteVisible(false)}
+                            >
+                                X
+                            </button>
+                        </>
+                    )}
+                </div>
+            </article>
+            <article className="mt-4">
+                <button
+                    type="button"
+                    className="p-3 border-2 rounded-sm"
+                    onClick={() => setIsAriaLiveAssertiveVisible(true)}
+                >
+                    Trigger Alert by ARIA-Live (assertive)
+                </button>
+                <div
+                    aria-live="assertive"
+                    className={clsx(
+                        "flex flex-row justify-between items-center",
+                        "p-3 border-2 rounded-sm bg-blue-200",
+                        "empty:hidden"
+                    )}
+                >
+                    {isAriaLiveAssertiveVisible && (
+                        <>
+                            <p>Hello, assertive.</p>
+                            <button
+                                type="button"
+                                className="p-3 border-2 rounded-sm"
+                                aria-label="Close"
+                                onClick={() => setIsAriaLiveAssertiveVisible(false)}
                             >
                                 X
                             </button>
